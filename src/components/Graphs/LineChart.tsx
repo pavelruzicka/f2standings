a6e8554b123a679da1628a331a3505d47e46bf98
Christian Ruigrok <christian@ruigrok.info>
2019-08-19 12:42:00 +0200

    cleanup graph looks

--- src/components/Graphs/LineChart.tsx
+++ src/components/Graphs/LineChart.tsx
@@ -5,9 +5,10 @@ import { IDriverBase } from "../../interfaces/Driver"
 import { teamColours } from "../../util/colours"
 import { ITeam } from "../../interfaces/Team"
 
-const width = 800
-const height = 500
+const width = 700
+const height = 400
 const padding = 30
+const legendSize = 20
 
 interface IProps {
   data: {
@@ -23,38 +24,60 @@ const SvgWrapper = styled.svg`
     font-family: "Red Hat Display", sans-serif;
   }
 
+  .grid .tick line {
+    opacity: 0.1;
+    stroke-width: 0.75;
+  }
+
   .axis-y {
-    .tick line {
+    .tick {
+      text {
+        transform: translateX(-10px);
+      }
     }
   }
 
   .axis-x {
-    font-weight: bold;
-
-    .tick line {
-      display: none;
+    .tick {
+      text {
+        font-weight: bold;
+        transform: translateY(10px);
+      }
     }
   }
+
+  .domain {
+    display: none;
+  }
 `
+
+function getTeamColor(driver: IDriverBase) {
+  return teamColours[driver.team] || "#000"
+}
+
+function isSecondDriver(teams: ITeam[], driver: IDriverBase) {
+  const team = teams.find(team => team.short === driver.team)
+  return team && team.drivers.indexOf(driver.short) === 1
+}
 
 export function LineChart({ data, races, teams }: IProps) {
   const svgRef = useRef<SVGSVGElement | null>(null)
 
   useEffect(() => {
-    const maxX = races.length
+    const maxX = races.length - 1
     const maxY = d3.max(data, d => d3.max(d.points, ([_, x]) => x)) || 0
 
     const xScale = d3
       .scaleLinear()
       .domain([0, maxX])
-      .range([0, width - padding * 2])
+      .range([0, width - legendSize])
 
     const yScale = d3
       .scaleLinear()
       .domain([0, maxY])
-      .range([height - padding * 2, 0])
+      .range([height, 0])
 
-    const xAxis = d3.axisBottom(xScale).tickFormat(x => races[x.valueOf()])
+    const xAxis = d3.axisBottom(xScale)
     const yAxis = d3.axisLeft(yScale)
 
     const svg = d3
@@ -62,7 +85,7 @@ export function LineChart({ data, races, teams }: IProps) {
       .attr("width", "100%")
       .attr("height", height)
       .attr("preserveAspectRatio", "xMinYMin meet")
-      .attr("viewBox", `0 0 ${width} ${height}`)
+      .attr("viewBox", `0 0 ${width + padding * 2} ${height + padding * 2}`)
 
     svg.selectAll("*").remove()
 
@@ -72,44 +95,75 @@ export function LineChart({ data, races, teams }: IProps) {
 
     const line = d3
       .line()
-      .curve(d3.curveBundle)
+      .curve(d3.curveBasis)
       .x(([x]) => xScale(x))
       .y(([_, y]) => yScale(y))
 
     for (const results of data) {
-      const team = teams.find(team => team.short === results.driver.team)
-
       chartGroup
         .append("path")
         .attr("fill", "none")
-        .attr("stroke", teamColours[results.driver.team] || "#000")
+        .attr("stroke", getTeamColor(results.driver))
         .attr("stroke-width", 2.25)
+        .attr("stroke-linecap", "round")
         .attr(
           "stroke-dasharray",
-          team && team.drivers.indexOf(results.driver.short) === 1
-            ? 11.3
-            : "initial"
+          isSecondDriver(teams, results.driver) ? "11 6" : "initial"
         )
-        .attr("data-shit", results.driver.short)
         .attr("d", line(results.points) || "")
-        .append("text")
-        .attr("x", 0)
-        .attr("y", 0)
-        .attr("font-family", "sans-serif")
-        .attr("font-size", "18px")
-        .attr("fill", "red")
-        .text("weewee")
     }
+
+    const legend = chartGroup
+      .append("g")
+      .attr("class", "legend")
+      .attr("transform", `translate(${width - legendSize + 20}, 0)`)
+
+    data.forEach((result, index) => {
+      const y = index * 18
+      const radius = 4
+
+      const teamColor = getTeamColor(result.driver)
+      const circle = legend
+        .append("circle")
+        .attr("r", radius)
+        .attr("cy", y - radius / 2 - 1.5)
+        .attr("cx", -radius * 2)
+        .attr("fill", "none")
+        .attr("stroke-width", 2)
+
+      if (isSecondDriver(teams, result.driver)) {
+        circle.attr("stroke", teamColor)
+        circle.attr("r", radius - 1)
+      } else {
+        circle.attr("fill", teamColor)
+      }
+
+      legend
+        .append("text")
+        .text(result.driver.short)
+        .attr("y", y)
+        .attr("font-size", 10)
+        .attr(
+          "title",
+          `${result.driver.name} ${result.driver.lastName} | ${result.driver.team}`
+        )
+    })
 
     chartGroup
       .append("g")
-      .attr("transform", `translate(0, ${height - padding * 2})`)
+      .attr("class", "grid grid-y")
+      .attr("transform", `translate(0, 0)`)
+      .call(yAxis.tickSize(-width + legendSize).tickFormat(() => ""))
+
+    chartGroup
+      .append("g")
+      .attr("transform", `translate(0, ${height})`)
       .attr("class", "axis axis-x")
-      .call(xAxis)
+      .call(xAxis.tickSize(0).tickFormat(s => races[s.valueOf()]))
     chartGroup
       .append("g")
       .attr("class", "axis axis-y")
-      .call(yAxis)
+      .call(yAxis.tickSize(0).tickFormat(s => String(s)))
   }, [data, races, svgRef, teams])
 
   return <SvgWrapper ref={svgRef} />
