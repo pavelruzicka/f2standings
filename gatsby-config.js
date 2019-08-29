df4346259a3540a518e83b32f45e53d65b0e70f3
Pavel Růžička <hello@pavelruzicka.com>
2019-08-29 19:47:27 +0200

    remove sharp plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,8 +6,6 @@ const config = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
